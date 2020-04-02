cbb7be04b6c7b6b791a9bb5893798ed4d07379cc
Andres Suarez <andresfelipe9619@gmail.com>
2020-04-01 22:15:59 -0500

    Fix wrong logic

--- MPI Colective Operations/Punto 4/promedioReduce.c
+++ MPI Colective Operations/Punto 4/promedioReduce.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#include <math.h>
 #include <assert.h>
+#include <time.h>
 
 // Creates an array of random numbers. Each number has a value from 0 - 1
 float *create_rand_nums(int num_elements) {
@@ -31,7 +31,7 @@ int main(int argc, char** argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
   // Create a random array of elements on all processes.
-  srand(time(NULL)*world_rank); // Seed the random number generator of processes uniquely
+  srand(time(NULL)*world_rank);   // Seed the random number generator to get different results each time for each processor
   float *rand_nums = NULL;
   rand_nums = create_rand_nums(num_elements_per_proc);
 
@@ -42,31 +42,19 @@ int main(int argc, char** argv) {
     local_sum += rand_nums[i];
   }
 
-  // Reduce all of the local sums into the global sum in order to
-  // calculate the mean
+  // Print the random numbers on each process
+  printf("Local sum for process %d - %f, avg = %f\n",
+         world_rank, local_sum, local_sum / num_elements_per_proc);
+
+  // Reduce all of the local sums into the global sum
   float global_sum;
-  MPI_Allreduce(&local_sum, &global_sum, 1, MPI_FLOAT, MPI_SUM,
-                MPI_COMM_WORLD);
-  float mean = global_sum / (num_elements_per_proc * world_size);
-
-  // Compute the local sum of the squared differences from the mean
-  float local_sq_diff = 0;
-  for (i = 0; i < num_elements_per_proc; i++) {
-    local_sq_diff += (rand_nums[i] - mean) * (rand_nums[i] - mean);
-  }
-
-  // Reduce the global sum of the squared differences to the root process
-  // and print off the answer
-  float global_sq_diff;
-  MPI_Reduce(&local_sq_diff, &global_sq_diff, 1, MPI_FLOAT, MPI_SUM, 0,
+  MPI_Reduce(&local_sum, &global_sum, 1, MPI_FLOAT, MPI_SUM, 0,
              MPI_COMM_WORLD);
 
-  // The standard deviation is the square root of the mean of the squared
-  // differences.
+  // Print the result
   if (world_rank == 0) {
-    float stddev = sqrt(global_sq_diff /
-                        (num_elements_per_proc * world_size));
-    printf("Mean - %f, Standard deviation = %f\n", mean, stddev);
+    printf("Total sum = %f, avg = %f\n", global_sum,
+           global_sum / (world_size * num_elements_per_proc));
   }
 
   // Clean up
